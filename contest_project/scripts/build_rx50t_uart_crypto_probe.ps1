c00f36b228fbbd1578863c5878f742ac39bc5cb9
LIZH-DEVE <4vf022580@gmail.com>
2026-09-21 10:34:15 +0800

    cleanup: make main Vivado build entrypoint portable

--- contest_project/scripts/build_rx50t_uart_crypto_probe.ps1
+++ contest_project/scripts/build_rx50t_uart_crypto_probe.ps1
@@ -2,14 +2,31 @@ $ErrorActionPreference = 'Stop'
 
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 $tcl = Join-Path $scriptDir 'create_rx50t_uart_crypto_probe_project.tcl'
-if ($env:VIVADO_BIN -and (Test-Path $env:VIVADO_BIN)) {
-    $vivado = $env:VIVADO_BIN
-} else {
-    $vivado = 'D:\Xilinx\Vivado\2024.1\bin\vivado.bat'
+
+$vivadoCandidates = @(
+    $env:VIVADO_BIN,
+    $env:RX50T_VIVADO_BAT,
+    'C:\Xilinx\Vivado\2024.1\bin\vivado.bat',
+    'D:\Xilinx\Vivado\2024.1\bin\vivado.bat'
+) | Where-Object { $_ }
+
+$vivado = $null
+foreach ($candidate in $vivadoCandidates) {
+    if (Test-Path $candidate) {
+        $vivado = $candidate
+        break
+    }
 }
 
-if (!(Test-Path $vivado)) {
-    throw "Vivado not found: $vivado"
+if (-not $vivado) {
+    $cmd = Get-Command vivado.bat -ErrorAction SilentlyContinue
+    if ($cmd) {
+        $vivado = $cmd.Source
+    }
+}
+
+if (-not $vivado) {
+    throw 'Vivado 2024.1 not found. Set VIVADO_BIN or RX50T_VIVADO_BAT, or add vivado.bat to PATH.'
 }
 
 if (!(Test-Path $tcl)) {
